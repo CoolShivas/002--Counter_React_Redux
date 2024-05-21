5bc26de051262072a1f1e9be0c67efa252f251af
CoolShivas <shivas2710cool00@gmail.com>
2024-05-21 23:21:39 +0530

    Functionality added on buttons with the help of action.type and dispatch

--- counter_react_redux/src/store/index.js
+++ counter_react_redux/src/store/index.js
@@ -1,20 +1,22 @@
-import {createStore} from "redux";
-
+import { createStore } from "redux";
 
 const INITIAL_VALUE = {
-    counter : 5
-};   
-
-
-const counterReducer = (store = INITIAL_VALUE, action) => {
-    console.log("Action Received", action);
-    // // Now, if you click on +1 we will get the output as Action Received {type: 'INCREMENT'};
-    // // Now, if you click on -1 we will get the output as Action Received {type: 'DECREMENT'};
-    return store;
+  counter: 5,
 };
 
+const counterReducer = (store = INITIAL_VALUE, action) => {
+  //console.log("Action Received", action);
+  // // Now, if you click on +1 we will get the output as Action Received {type: 'INCREMENT'};
+  // // Now, if you click on -1 we will get the output as Action Received {type: 'DECREMENT'};
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { counter: store.counter - 1 };
+  } else {
+    return store;
+  }
+};
 
 const counterStore = createStore(counterReducer);
-
 
 export default counterStore;
