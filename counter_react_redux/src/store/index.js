aa29e26f0cc1cc7209dc5300040ddf3d51860c67
CoolShivas <shivas2710cool00@gmail.com>
2024-05-21 19:11:19 +0530

    Assigning the Initial value to Store

--- counter_react_redux/src/store/index.js
+++ counter_react_redux/src/store/index.js
@@ -1,7 +1,12 @@
 import {createStore} from "redux";
 
 
-const counterReducer = (store, action) => {
+const INITIAL_VALUE = {
+    counter : 0
+};   
+
+
+const counterReducer = (store = INITIAL_VALUE, action) => {
     return store;
 };
 
