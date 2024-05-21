e41eb3dab632fddbff0be0f32b4fca0d84bba760
CoolShivas <shivas2710cool00@gmail.com>
2024-05-21 18:07:47 +0530

    Adding Modal or Card

--- counter_react_redux/src/App.jsx
+++ counter_react_redux/src/App.jsx
@@ -1,10 +1,13 @@
+import Modal from "./components/Modal";
 import Buttons from "./components/Buttons";
 import DisplayValue from "./components/DisplayValue";
 import Header from "./components/Header";
 
 const App = () => {
   return (
+
     <div className="px-4 py-5 my-5 text-center">
+      <Modal></Modal>
       <Header></Header>
       <div className="col-lg-6 mx-auto">
         <DisplayValue></DisplayValue>
