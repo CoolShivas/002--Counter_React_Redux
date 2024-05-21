bb0a6e0efc01168177ff16c21103537cf77f3c2f
CoolShivas <shivas2710cool00@gmail.com>
2024-05-21 18:10:18 +0530

    Wrapping the Modal or Card

--- counter_react_redux/src/App.jsx
+++ counter_react_redux/src/App.jsx
@@ -7,14 +7,15 @@ const App = () => {
   return (
 
     <div className="px-4 py-5 my-5 text-center">
-      <Modal></Modal>
-      <Header></Header>
-      <div className="col-lg-6 mx-auto">
-        <DisplayValue></DisplayValue>
-        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <Buttons></Buttons>
+      <Modal>
+        <Header></Header>
+        <div className="col-lg-6 mx-auto">
+          <DisplayValue></DisplayValue>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <Buttons></Buttons>
+          </div>
         </div>
-      </div>
+      </Modal>
     </div>
   )
 }
