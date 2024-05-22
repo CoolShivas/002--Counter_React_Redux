8f3e85f8a1a074a7340e8a6fda83a5f5cb735986
CoolShivas <shivas2710cool00@gmail.com>
2024-05-22 10:35:20 +0530

    Use of Spread Operator

--- counter_react_redux/src/store/index.js
+++ counter_react_redux/src/store/index.js
@@ -12,27 +12,32 @@ const counterReducer = (store = INITIAL_VALUE, action) => {
   // // Now, if you click on -1 we will get the output as Action Received {type: 'DECREMENT'};
   if (action.type === "INCREMENT") {
     return {
+      ...store, // It will print the previous key and value;
       counter: store.counter + 1,
-      privacy: store.privacy,
+      // privacy: store.privacy,// Now, with the help of spread operator there is no need of mentioning privacy or counter if spread operatore used with store;
     };
   } else if (action.type === "DECREMENT") {
     return {
+      ...store,
       counter: store.counter - 1,
-      privacy: store.privacy,
+      // privacy: store.privacy,
     };
   } else if (action.type === "ADDITION") {
     return {
+      ...store,
       counter: store.counter + action.payload.num,
-      privacy: store.privacy,
+      // privacy: store.privacy,
     };
   } else if (action.type === "SUBTRACTION") {
     return {
+      ...store,
       counter: store.counter - action.payload.num,
-      privacy: store.privacy,
+      // privacy: store.privacy,
     };
   } else if (action.type === "PRIVACY_TOGGLE") {
     return {
-      counter: store.counter, // And Counter value is remain same here as it is;
+      ...store,
+      // counter: store.counter, // And Counter value is remain same here as it is;
       privacy: !store.privacy, // Now privacy is true ;
     };
   } else {
